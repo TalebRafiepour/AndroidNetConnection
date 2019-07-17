e792dfcad23f7fa0335a981f2d8123df53236155
taleb <taleb.r75@gmail.com>
2019-07-17 14:35:00 +0430

    add json post(none multipart post)

--- andronetconnection/src/main/java/com/taleb/netconnection/NetworkConnection.kt
+++ andronetconnection/src/main/java/com/taleb/netconnection/NetworkConnection.kt
@@ -4,11 +4,13 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.os.Handler
 import android.os.StrictMode
+import android.util.Log
 import okhttp3.*
 import org.json.JSONObject
 import java.io.IOException
 import java.util.*
 import java.util.concurrent.TimeUnit
+import kotlin.collections.HashMap
 
 
 class NetworkConnection(private val delegate: INetworkConnection? = null, private val context: Context) {
@@ -26,7 +28,7 @@ class NetworkConnection(private val delegate: INetworkConnection? = null, privat
     fun post(parameters: HashMap<String, String>, url: String, requestCode: Int) {
         //todo (add post type (json or multipart) in the future if needed)
         if (!verifyAvailableNetwork(context)) {
-            delegate?.onFailure(requestCode, com.taleb.netconnection.Error(NETWORK_NOT_REACHABLE_ERROR_CODE,context.getString(R.string.verify_you_network_connectivity)))
+            delegate?.onFailure(requestCode, Error(NETWORK_NOT_REACHABLE_ERROR_CODE,context.getString(R.string.verify_you_network_connectivity)))
             return
         }
         val requestBody = MultipartBody.Builder()
@@ -40,21 +42,67 @@ class NetworkConnection(private val delegate: INetworkConnection? = null, privat
             .build()
         client.newCall(request).enqueue(object : Callback {
 
-            override fun onFailure(call: okhttp3.Call, e: IOException) {
+            override fun onFailure(call: Call, e: IOException) {
                 //todo handle error
                 val mainHandler = Handler(context.mainLooper)
                 mainHandler.post {
-                    kotlin.run {
+                    run {
                         e.printStackTrace()
-                        delegate?.onFailure(requestCode, com.taleb.netconnection.Error(API_CALL_FAIL_ERROR_CODE,context.getString(R.string.server_connection_error)))
+                        delegate?.onFailure(requestCode, Error(API_CALL_FAIL_ERROR_CODE,context.getString(R.string.server_connection_error)))
                     }
                 }
             }
 
-            override fun onResponse(call: okhttp3.Call, response: Response) {
+            override fun onResponse(call: Call, response: Response) {
                 val mainHandler = Handler(context.mainLooper)
                 mainHandler.post {
-                    kotlin.run {
+                    run {
+                        handleResponse(response, requestCode)
+                    }
+                }
+            }
+        })
+    }
+
+    fun post(url: String, requestCode: Int = 0,bodyJsonString: String,headers : HashMap<String,String>? = null) {
+
+        if (!verifyAvailableNetwork(context)) {
+            delegate?.onFailure(requestCode, Error(NETWORK_NOT_REACHABLE_ERROR_CODE,context.getString(R.string.verify_you_network_connectivity)))
+            return
+        }
+
+
+        val json = MediaType.parse("application/json; charset=utf-8")
+        val body = RequestBody.create(json, bodyJsonString)
+        val requestBuilder = Request.Builder()
+            .addHeader("Content-Type", "application/json")
+
+        if (headers != null){
+            for ((key,value) in headers){
+                requestBuilder.addHeader(key,value)
+            }
+        }
+
+
+        val request = requestBuilder.url(url).post(body).build()
+
+        client.newCall(request).enqueue(object : Callback {
+
+            override fun onFailure(call: Call, e: IOException) {
+                //todo handle error
+                val mainHandler = Handler(context.mainLooper)
+                mainHandler.post {
+                    run {
+                        e.printStackTrace()
+                        delegate?.onFailure(requestCode, Error(API_CALL_FAIL_ERROR_CODE,context.getString(R.string.server_connection_error)))
+                    }
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val mainHandler = Handler(context.mainLooper)
+                mainHandler.post {
+                    run {
                         handleResponse(response, requestCode)
                     }
                 }
@@ -83,19 +131,19 @@ class NetworkConnection(private val delegate: INetworkConnection? = null, privat
                 }
                 401 -> {
                     //todo (handle 401 response message to show for user)
-                    delegate?.onFailure(requestCode, com.taleb.netconnection.Error(401,response.message()))
+                    delegate?.onFailure(requestCode, Error(401,response.message()))
                 }
                 403 -> {
                     //todo (handle 401 response message to show for user)
-                    delegate?.onFailure(requestCode, com.taleb.netconnection.Error(403,response.message()))
+                    delegate?.onFailure(requestCode, Error(403,response.message()))
                 }
                 404 -> {
-                    delegate?.onFailure(requestCode, com.taleb.netconnection.Error(404,context.getString(R.string.server_connection_error)))
+                    delegate?.onFailure(requestCode, Error(404,context.getString(R.string.server_connection_error)))
                 }
             }
         } catch (e: Exception) {
             e.printStackTrace()
-            delegate?.onFailure(requestCode, com.taleb.netconnection.Error(NONE_DEFINED_EXCEPTION_ERROR_CODE,context.getString(R.string.exception_in_gathering_info)))
+            delegate?.onFailure(requestCode, Error(NONE_DEFINED_EXCEPTION_ERROR_CODE,context.getString(R.string.exception_in_gathering_info)))
         }
     }
 
