94eb63fc68866fef103f87439381efe68b6db9d4
Taleb <taleb.r75@gmail.com>
2019-07-23 00:51:17 +0430

    add post multipart with Json Object support

--- andronetconnection/src/main/java/com/taleb/netconnection/NetworkConnection.kt
+++ andronetconnection/src/main/java/com/taleb/netconnection/NetworkConnection.kt
@@ -4,13 +4,10 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.os.Handler
 import android.os.StrictMode
-import android.util.Log
 import okhttp3.*
 import org.json.JSONObject
 import java.io.IOException
-import java.util.*
 import java.util.concurrent.TimeUnit
-import kotlin.collections.HashMap
 
 
 class NetworkConnection(private val delegate: INetworkConnection? = null, private val context: Context) {
@@ -25,8 +22,11 @@ class NetworkConnection(private val delegate: INetworkConnection? = null, privat
         StrictMode.setThreadPolicy(policy)
     }
 
+
+    /*
+    * post multipart
+    * */
     fun post(parameters: HashMap<String, String>, url: String, requestCode: Int) {
-        //todo (add post type (json or multipart) in the future if needed)
         if (!verifyAvailableNetwork(context)) {
             delegate?.onFailure(requestCode, Error(NETWORK_NOT_REACHABLE_ERROR_CODE,context.getString(R.string.verify_you_network_connectivity)))
             return
@@ -64,6 +64,54 @@ class NetworkConnection(private val delegate: INetworkConnection? = null, privat
         })
     }
 
+
+    /*
+    * post multipart with json object key value param
+    * */
+    fun post(parameters: JSONObject, url: String, requestCode: Int) {
+        if (!verifyAvailableNetwork(context)) {
+            delegate?.onFailure(requestCode, Error(NETWORK_NOT_REACHABLE_ERROR_CODE,context.getString(R.string.verify_you_network_connectivity)))
+            return
+        }
+        val requestBody = MultipartBody.Builder()
+            .setType(MultipartBody.FORM)
+        val paramKeys = parameters.keys()
+        for (key in paramKeys) {
+            requestBody.addFormDataPart(key, parameters.get(key).toString())
+        }
+        val request = Request.Builder()
+            .url(url)
+            .post(requestBody.build())
+            .build()
+        client.newCall(request).enqueue(object : Callback {
+
+            override fun onFailure(call: Call, e: IOException) {
+                //todo handle error
+                val mainHandler = Handler(context.mainLooper)
+                mainHandler.post {
+                    run {
+                        e.printStackTrace()
+                        delegate?.onFailure(requestCode, Error(API_CALL_FAIL_ERROR_CODE,context.getString(R.string.server_connection_error)))
+                    }
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val mainHandler = Handler(context.mainLooper)
+                mainHandler.post {
+                    run {
+                        handleResponse(response, requestCode)
+                    }
+                }
+            }
+        })
+    }
+
+
+
+    /*
+    * post none multipart
+    * */
     fun post(url: String, requestCode: Int = 0,bodyJsonString: String,headers : HashMap<String,String>? = null) {
 
         if (!verifyAvailableNetwork(context)) {
